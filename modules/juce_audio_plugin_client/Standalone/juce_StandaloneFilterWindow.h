cc7581b532e9f887bcfe9c0d40bcb7627d6eef5a
Chris Lucas <chris@3sigmaaudio.com>
2025-08-17 17:59:29 -0700

    feat: Allow single input/output channel selection for standalone plugin window.

--- modules/juce_audio_plugin_client/Standalone/juce_StandaloneFilterWindow.h
+++ modules/juce_audio_plugin_client/Standalone/juce_StandaloneFilterWindow.h
@@ -540,7 +540,7 @@ private:
                               0, maxAudioOutputChannels,
                               true,
                               (pluginHolder.processor.get() != nullptr && pluginHolder.processor->producesMidi()),
-                              true, false),
+                              false, false),
               shouldMuteLabel  ("Feedback Loop:", "Feedback Loop:"),
               shouldMuteButton ("Mute audio input")
         {
